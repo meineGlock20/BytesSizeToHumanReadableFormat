41afa4781f83f295d89137ba4d6a9f684f996967
Dana King <bushido101@hotmail.com>
2024-10-07 17:52:23 -0300

    Refactor code to add MIT License header and copyright notice

--- ConsoleDemo/Program.cs
+++ ConsoleDemo/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using System;
+using System.Globalization;
+using BytesSizeToHumanReadableFormat;
+
+// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+long bytes = 1000;
+Console.WriteLine(bytes.BytesToHumanReadableFormat(RoundToDecimalPlaces.Two, ""));
